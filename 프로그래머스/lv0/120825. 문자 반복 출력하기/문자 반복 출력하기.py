0e734a058a24af4f936e7e777abdc546897be201
진민지 <67037321+realmingji@users.noreply.github.com>
2023-03-06 10:51:02 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
+++ 프로그래머스/lv0/120825. 문자 반복 출력하기/문자 반복 출력하기.py
@@ -1,7 +1,7 @@
 def solution(my_string, n):
     answer = ''
-    my_string = list(my_string)
+    str = list(my_string)
     
-    for i in range(len(my_string)):
-        my_string[i] = my_string[i] * n
-    return answer.join(my_string)
+    for i in range(len(str)):
+        str[i] = str[i] * n
+    return answer.join(str)
