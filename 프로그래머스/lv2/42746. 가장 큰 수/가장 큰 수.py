4e16f8c238bb4d1cd3b8468371f23f3a3c6be421
진민지 <67037321+realmingji@users.noreply.github.com>
2023-03-31 17:05:37 +0900

    Update 가장 큰 수.py

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -2,6 +2,6 @@ def solution(numbers):
     # [6, 10, 2]를 [6102, 6210, 1062, 1026, 2610, 2106]
     # 큰 수는 6210 
     numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: (x * 4)[:4], reverse=True)
+    numbers.sort(key=lambda x: (x * 3), reverse=True)
     answer = str(int(''.join(numbers)))
     return answer
