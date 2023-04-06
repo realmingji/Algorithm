d7aad753a671fca265904e72b6468aa46e31c5ee
진민지 <67037321+realmingji@users.noreply.github.com>
2023-04-06 18:19:40 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -2,6 +2,7 @@ def solution(numbers):
     # [6, 10, 2]를 [6102, 6210, 1062, 1026, 2610, 2106]
     # 큰 수는 6210 
     numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: (x * 3), reverse=True)
+    # 인터넷 검색해봄
+    numbers.sort(key = lambda x : x * 3, reverse=True) # 역순정렬
     answer = str(int(''.join(numbers)))
     return answer
