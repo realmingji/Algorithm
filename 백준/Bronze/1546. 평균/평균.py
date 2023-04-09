c60c31ddda9fb47042b594e7847d205e00337a64
진민지 <67037321+realmingji@users.noreply.github.com>
2023-04-09 21:32:21 +0900

    [Bronze I] Title: 평균, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -3,6 +3,6 @@ arr=list(map(int, input().split()))
 sum=0
 
 for i in range(len(arr)):
-    sum=sum+arr[i]/max(arr)*100
-avg=sum/len(arr)
+    sum=sum+arr[i]/max(arr)*100    # 50/70*100 = 71.43
+avg=sum/len(arr)    # 평균 구하기
 print(avg)
