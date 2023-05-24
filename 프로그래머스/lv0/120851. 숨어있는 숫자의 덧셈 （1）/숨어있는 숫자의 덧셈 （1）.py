1e887932785adfa97053157e8f402e098a9d0e5b
진민지 <67037321+realmingji@users.noreply.github.com>
2023-05-25 00:48:52 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -1,11 +1,24 @@
-import re
+# import re
+
+# def solution(my_string):
+#     arr=[]
+#     sum = 0
+    
+#     result = re.sub(r'[^0-9]', '', my_string)
+    
+#     for i in range(len(result)):
+#         sum = sum + int(result[i])
+#     return sum
+
 
 def solution(my_string):
-    arr=[]
-    sum = 0
-    
-    result = re.sub(r'[^0-9]', '', my_string)
-    
-    for i in range(len(result)):
-        sum = sum + int(result[i])
-    return sum
+    answer = 0
+    number = ['0','1','2','3','4','5','6','7','8','9']
+
+    for i in range(len(my_string)):
+        if my_string[i] in number:
+            answer += int(my_string[i])
+        else:
+            continue
+
+    return answer
