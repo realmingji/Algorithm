64f92b47490e08a56f5e39fe91bfce364d220db1
진민지 <67037321+realmingji@users.noreply.github.com>
2023-05-31 17:10:33 +0900

    [Bronze I] Title: 약수들의 합, Time: 100 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -0,0 +1,17 @@
+while True:
+	a=[]
+	num = 0
+	n=int(input())
+	
+	if n == -1:
+		break
+	
+	for i in range(1, n):
+		if n % i == 0:
+			a.append(i)
+	if sum(a) == n:
+		num = ' + '.join(map(str, a))
+		print(n, '=', num)
+	else:
+		re = f'{n} is NOT perfect.'
+		print(re)
