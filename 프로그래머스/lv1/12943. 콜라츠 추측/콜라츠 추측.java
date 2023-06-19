f016e1e639b22019ace309271f1f90f1f4c890c4
진민지 <67037321+realmingji@users.noreply.github.com>
2023-06-19 23:19:47 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        // 입력 수 => 짝수면 2로 나누기
+        // 입력 수 => 홀수면 3을 곱하고 1더하기
+        // 1이 될 때까지 반복
+        int a = 0,b=0, sum=0;
+        while(num != 1){
+             // num이 짝수인 경우
+            if(num%2==0) {       // num이 6이면?
+                num = num/2;      // a는 몫   
+                answer++;
+            } 
+            else if(num%2==1){       // 홀수
+                num=num*3+1;   
+                answer++;
+            }
+            else{       // 500번 반복할 때까지 1이 되지않으면?
+                return -1;
+            }
+        }
+        
+        return answer;
+    }
+}
