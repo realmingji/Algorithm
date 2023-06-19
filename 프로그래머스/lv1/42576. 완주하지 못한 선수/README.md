52cbabe6f423f413340d7e3338de8edfb1141c55
진민지 <67037321+realmingji@users.noreply.github.com>
2023-06-19 20:17:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 345.74 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 95.5 MB, 시간: 271.73 ms
+메모리: 95.2 MB, 시간: 345.74 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,52 +1,44 @@
-// import java.util.Arrays;
-// import java.util.*;
-// class Solution {
-//     public String solution(String[] participant, String[] completion) {
-//         String answer = "";
-        
-//         Arrays.sort(participant);       // participant 배열 정렬 
-//         Arrays.sort(completion);       // completion 배열 정렬 
-// //         System.out.println(Arrays.toString(participant));
-// //         System.out.println(Arrays.toString(completion));
-        
-//         // Set set =new HashSet();
-//         // for(int j=0; j<participant.length; j++){
-//         //     set.add(participant[j]);
-//         //     System.out.println(set);
-//         // }
-        
-//         // 중복 제거된 배열 생성
-//         HashSet<String> set1 = new HashSet<>(Arrays.asList(participant));
-//         String[] setparticipants = set1.toArray(new String[set1.size()]);
-        
-//         // HashSet<String> set2 = new HashSet<>(Arrays.asList(completion));
-//         // String[] setcompletion = set2.toArray(new String[set2.size()]);
-        
-//          // 배열 정렬
-//         Arrays.sort(setparticipants);
-// //         Arrays.sort(setcompletion);
-        
-//         for(int i=0; i<completion.length; i++){ // i가 0부터 시작해서 completion 길이까지 반복해라
-//             if(!(setparticipants[i].equals(completion[i]))){  // 같지 않으면
-//                 return setparticipants[i];  //  participant[i] 리턴해라
-//             }
-//             return participant[setparticipants.length - 1];
-//         }
-//         return answer;
-//     }
-// }
+import java.util.Arrays;
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        Arrays.sort(participant);
-        Arrays.sort(completion);
+        String answer = "";      
+        Arrays.sort(participant);       // participant 배열 정렬 
+        Arrays.sort(completion);       // completion 배열 정렬 
+        // System.out.println(Arrays.toString(participant));
+        // System.out.println(Arrays.toString(completion));
+        
+        // 중복 제거된 배열 생성
+        // HashSet<String> set1 = new HashSet<>(Arrays.asList(participant));
+        // String[] setparticipants = set1.toArray(new String[set1.size()]);
+        
+        // HashSet<String> set2 = new HashSet<>(Arrays.asList(completion));
+        // String[] setcompletion = set2.toArray(new String[set2.size()]);
+        
+        //  // 배열 정렬
+        // Arrays.sort(setparticipants);
+//         Arrays.sort(setcompletion);
         
         for(int i =0; i < completion.length; i++){
-            if(!(participant[i].equals(completion[i]))){
+            if(!participant[i].equals(completion[i])){
                 return participant[i];
             }
         }
         return participant[participant.length-1];
     }
 }
+// import java.util.*;
+// class Solution {
+//     public String solution(String[] participant, String[] completion) {
+//         String answer = "";
+//         Arrays.sort(participant);
+//         Arrays.sort(completion);
+        
+//         for(int i =0; i < completion.length; i++){
+//             if(!(participant[i].equals(completion[i]))){
+//                 return participant[i];
+//             }
+//         }
+//         return participant[participant.length-1];
+//     }
+// }
