f9da357961eea229849a872ec803c3f9b5bbb8c9
진민지 <67037321+realmingji@users.noreply.github.com>
2023-06-19 19:16:46 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -5,19 +5,24 @@ class Solution {
         int[] answer = new int[2];
         
         for(int i=0; i<lottos.length; i++){
-            if(lottos[i] == 0){
+            for(int j=0; j<win_nums.length; j++){
+                if (lottos[i] == win_nums[j]){
+                    cnt++;
+                }            
+            }
+            if(lottos[i]==0){
                 zero++;
-            } else {
-                for(int j=0; j<win_nums.length; j++){
-                    if (lottos[i] == win_nums[j]){
-                        cnt++;
-                        break; // 이미 맞는 숫자를 찾았으므로 다음 번호로 넘어감
-                    }            
-                }
-            }          
+            }           
         }
-
-        // 최고 자리
+// 순위           6 - (맞은 갯수 + 1)
+// 6개 중 6개 맞음 => 1등     6-6+1
+// 6개 중 5개 맞음 => 2등     6-5+1
+// 6개 중 4개 맞음 => 3등     6-4+1
+// 6개 중 3개 맞음 => 4등     6-3+1
+// 6개 중 2개 맞음 => 5등     6-2+1
+// 6개 중 1개 맞거나 0개 맞으면 => 낙첨 6-1+1
+        
+       // 최고 자리
         answer[0] = getRank(cnt + zero);
         
         // 최저 자리
