ca091575226bb6a4b37ad1891c8d68fa9dea4076
진민지 <67037321+realmingji@users.noreply.github.com>
2023-06-22 01:00:39 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 10.16 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,5 +1,6 @@
 // 먼저 공백을 만날때까지 돌려라
 // 만약에 공백을 만났다? => 
+// 다시 풀어보기
 class Solution {
     public String solution(String s) {
         String answer = "";
@@ -7,13 +8,16 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-
+ 
             if (c == ' ') {
                 answer += c;
+                System.out.print(answer);
                 x = 0;
+                // System.out.print(x);
             } else {
                 if (x % 2 == 0) {
                     answer += Character.toUpperCase(c);
+                    // System.out.print(x);
                 } else {
                     answer += Character.toLowerCase(c);
                 }
