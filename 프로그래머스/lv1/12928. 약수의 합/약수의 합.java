778d775d50a24906dce11042880bb8758b3a20e7
진민지 <67037321+realmingji@users.noreply.github.com>
2023-10-06 16:30:41 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -1,9 +1,10 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for (int i=1; i<n+1; i++){
-            if(n%i==0){
-                answer+=i;
+        
+        for(int i = 1; i < n + 1; i++){
+            if(n % i == 0) {
+                answer = answer + i;
             }
         }
         return answer;
