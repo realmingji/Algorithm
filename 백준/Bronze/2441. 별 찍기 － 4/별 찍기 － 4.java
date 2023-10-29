2a9a9ca3c6f76760116bc08754427acffec1e5ba
진민지 <67037321+realmingji@users.noreply.github.com>
2023-10-29 15:31:07 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 372 ms, Memory: 18340 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k < n-i; k++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
