6ea9ba0da24d580f78a5371578e8577a8d2287c0
진민지 <67037321+realmingji@users.noreply.github.com>
2024-02-06 21:20:58 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -2,10 +2,9 @@ def solution(array, commands):
     # array=[1,5,2,6,3,7,4], i=2, j=5, k=3
     # i번째부터 j번째까지 자르기
     # 배열 정렬 후 k번째 숫자 구하기
-    num = []
+    count = []
     for i, j, k in commands:
-        # slice = array[i-1:j]
-        # slice.sort()
-        # count.append(slice[k-1])
-        num.append(sorted(array[i-1:j])[k-1]) # 한줄로 표현하는 방법
-    return num
+        slice = array[i-1:j]
+        slice.sort()
+        count.append(slice[k-1])
+    return count
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,6 +1,6 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=python3) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 2일 20:49:25
+2024년 2월 2일 21:20:49
 
 ### 문제 설명
 
