0dd69f51ab45667cc87241184698bbe605b12434
진민지 <67037321+realmingji@users.noreply.github.com>
2024-02-06 21:57:58 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -3,9 +3,7 @@ class Solution {
         int answer = 1;
         
         for(int i = 0; i < box.length; i++){
-            if(box[i] % n == 0 || box[i] % n == 1 || box[i] % n == 2 || box[i] % n == 3 || box[i] % n == 4 || box[i] % n == 5 || box[i] % n == 6 || box[i] % n == 7 || box[i] % n == 8 || box[i] % n == 9){
-                answer *= box[i] / n;
-            }
+            answer *= box[i] / n;
         }
         return answer; 
     }
