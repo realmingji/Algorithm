b4371181bc51505dd4bd48ab78b5ca56f7c1507a
진민지 <67037321+realmingji@users.noreply.github.com>
2024-03-06 10:25:17 +0900

    [level 0] Title: 약수 구하기, Time: 0.02 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] solution(int n) {
+        int cnt = 0;
+        for(int i=1; i<n+1; i++){
+            if(n%i==0){
+                cnt++;
+            }
+        }
+        
+        int[] answer = new int[cnt];
+        int index = 0;
+        for(int i=1; i<n+1; i++){
+            if(n%i==0) {
+                answer[index] = i;
+                index++;
+            }
+        }
+        return answer;
+    }
+}
