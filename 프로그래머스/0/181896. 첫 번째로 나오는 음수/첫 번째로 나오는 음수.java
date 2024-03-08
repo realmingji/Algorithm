7551a2063f3d16e79ab32dd4e9a0ddf8a27c78c4
진민지 <67037321+realmingji@users.noreply.github.com>
2024-03-08 14:21:55 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0; i<num_list.length; i++) {
             if(num_list[i] <= 0) {
                 answer = i;
-                break;
+                return answer;
             }
         }
         return answer;
